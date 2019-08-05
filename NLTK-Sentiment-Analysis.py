f9cb1f97186ba4cb5bc8b1d97336f710ec188982
Brian <cobo.brian97@gmail.com>
2019-08-05 01:22:01 -0500

    Helped streamline the analysis process
    I created a main function to help guide the process from extracting data on the internet, to providing sentiment analysis of it. I just need to make it store into the csv file

--- NLTK-Sentiment-Analysis.py
+++ NLTK-Sentiment-Analysis.py
@@ -15,29 +15,48 @@ import re
 # http://www.nltk.org/howto/sentiment.html
 # https://www.dataquest.io/blog/web-scraping-tutorial-python/
 
+
+def add_row_to_saved_article_results_dataframe(article_info):
+    if check_for_existing_article_results(article_info['URL']) == False:
+        pass
+    else:
+        print('Article Info Already Saved')
+
+
+def create_saved_article_results_dataframe():
+    article = pd.DataFrame(columns=['URL',
+                                    'Title',
+                                    'Company',
+                                    'Author',
+                                    'Date',
+                                    'Time',
+                                    'Overall',
+                                    'Positive',
+                                    'Negative',
+                                    'Neutral'])
+    article.to_csv(os.getcwd() + '/Article_Sentiment_Results/Sentiment_Results.csv')
+    return article
+
+def return_article_results_dataframe():
+    article_result_file_path = os.getcwd() + '/Article_Sentiment_Results/Sentiment_Results.csv'
+    if os.path.exists(article_result_file_path):
+        saved_article_results = pd.read_csv(article_result_file_path)
+        if len(saved_article_results) > 0:
+            return saved_article_results
+    return create_saved_article_results_dataframe()
+
+
 def check_for_existing_article_results(URL_to_check_for):
     article_result_file_path = os.getcwd() + '/Article_Sentiment_Results/Sentiment_Results.csv'
     if os.path.exists(article_result_file_path):
         article = pd.read_csv(article_result_file_path)
         data = article[(article.URL == URL_to_check_for)]
         if len(data) > 0:
-            return data
+            return True
         else:
-            return None
+            return False
+    return False
 
-    else:
-        article = pd.DataFrame(columns=['URL',
-                                        'Title',
-                                        'Company',
-                                        'Author',
-                                        'Date',
-                                        'Time',
-                                        'Overall',
-                                        'Positive',
-                                        'Negative',
-                                        'Neutral'])
-        article.to_csv(os.getcwd() + '/Article_Sentiment_Results/Sentiment_Results.csv')
-        return None
 
 def filter_out_stop_words(tokenized_words):
     stop_words = set(stopwords.words("english"))
@@ -63,27 +82,20 @@ def prepare_article():
 
     return tokenized_sentence
 
-def get_sentiment_analysis():
-    if check_for_existing_article_results() == None:
-        print('Article Already Exists')
-        exit(0)
-    else:
-        article = os.getcwd() + '/Articles/Apple-Article.txt'
-
-    print('\nScores are rated from -1 (Highly Negative) to 1 (Highly Positive)')
+def get_sentiment_analysis(article_info):
+    article = article_info['Article']
     sid = SentimentIntensityAnalyzer()
     total_sentiment_values = {'Compound': 0, 'Positive': 0, 'Negative': 0, 'Neutral': 0}
     average_sentiment_values = {'Compound': 0, 'Positive': 0, 'Negative': 0, 'Neutral': 0}
     number_of_sentences = len(article)
 
     for sentence in article:
-        print('\nSentence:', sentence)
         results = sid.polarity_scores(sentence)
         # print('Overall:', results['compound'],
         #       ' Positive:', results['pos'],
         #       ' Negative:', results['neg'],
         #       ' Neutral:', results['neu'])
-        total_sentiment_values['Compound'] += results ['compound']
+        total_sentiment_values['Compound'] += results['compound']
         total_sentiment_values['Positive'] += results['pos']
         total_sentiment_values['Negative'] += results['neg']
         total_sentiment_values['Neutral'] += results['neu']
@@ -93,26 +105,21 @@ def get_sentiment_analysis():
     average_sentiment_values['Negative'] = float(total_sentiment_values['Negative'] / number_of_sentences)
     average_sentiment_values['Neutral'] = float(total_sentiment_values['Neutral'] / number_of_sentences)
 
-    print('\nArticle Results:'
-          ' Overall:', average_sentiment_values['Compound'],
-          ' Positive:', average_sentiment_values['Positive'],
-          ' Negative:', average_sentiment_values['Negative'],
-          ' Neutral:', average_sentiment_values['Neutral'])
+    article_info['Overall_SA'] = average_sentiment_values['Compound']
+    article_info['Positive_SA'] = average_sentiment_values['Positive']
+    article_info['Negative_SA'] = average_sentiment_values['Negative']
+    article_info['Neutral_SA'] = average_sentiment_values['Neutral']
+    article_info.pop('Article', None)
 
-# Currently only works for IBTimes.com articles
-def scrape_article_from_web(article_URL):
-    page = requests.get(article_URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    #print(soup.prettify())
-    #Company
+    return article_info
 
+def find_title_from_article(soup):
+    return soup.find_all('title')[0].get_text()
 
-    # Find Article Title
-    title = soup.find_all('title')[0].get_text()
+def find_author_from_article(soup):
+    return soup.find_all('span', itemprop='name')[0].get_text()
 
-    #Find Author
-    author = soup.find_all('span', itemprop='name')[0].get_text() #property='article:author'
-
+def find_publish_date_and_time_from_article(soup):
     # Find Article Date and Time
     time = soup.find_all('time', itemprop='datePublished')
     published_time = ''
@@ -121,16 +128,18 @@ def scrape_article_from_web(article_URL):
     published_time = published_time.split()
     date_published = published_time[0]
     time_published = (' ').join(published_time[2:4])
+    return (date_published, time_published)
 
-    # Find Article Content
+def find_article_content(soup):
     tag = soup.find_all('p')
     article = ''
     for i in tag:
         article += (i.get_text())
-    article = [article]
+    return [article]
 
+def find_company_symbol_from_article(soup, title, article_content):
     # Find the Company Symbol the article talks about
-    company_symbol = ''
+    company_symbol = None
     company = soup.find_all('meta')
     for i in company:
         if 'keywords' in str(i):
@@ -145,13 +154,29 @@ def scrape_article_from_web(article_URL):
                 try:
                     symbols = search_for_company_symbol(key, automated=True)
                     if (key in title.lower() and
-                            key in article[0].lower() and
+                            key in article_content[0].lower() and
                             key in symbols.Name.iloc[0].lower()):
                         company_symbol = symbols.Symbol.iloc[0]
-                    break
+                        break
                 except Exception as e:
                     print("ERROR:", e)
             break
+    return company_symbol
+
+def print_dictionary(dict):
+    for key, value in dict.items():
+        print(key, ':', value)
+
+def scrape_article_from_web(article_URL):
+    page = requests.get(article_URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    #print(soup.prettify())
+
+    title = find_title_from_article(soup)
+    author = find_author_from_article(soup)
+    date_published, time_published = find_publish_date_and_time_from_article(soup)
+    article_content = find_article_content(soup)
+    company_symbol = find_company_symbol_from_article(soup, title, article_content)
 
     article_extracted_info = {'Title' : title,
                               'Author': author,
@@ -159,14 +184,27 @@ def scrape_article_from_web(article_URL):
                               'Time_Published' : time_published,
                               'Symbol' : company_symbol,
                               'URL' : article_URL,
-                              'Article' : article}
-    print()
-    for key, value in article_extracted_info.items():
-        print(key, ':', value)
+                              'Article' : article_content}
+
     return(article_extracted_info)
 
 
 url = 'https://www.ibtimes.com/apple-stock-4-q3-earnings-beat-despite-low-iphone-sales-2809781?ft=2gh92&utm_source=Robinhood&utm_medium=Site&utm_campaign=Partnerships'
-scrape_article_from_web(url)
-#get_sentiment_analysis()
+url2 = 'https://www.ibtimes.com/does-starbucks-want-become-tech-company-2810671'
+url3 = 'https://www.ibtimes.com/tesla-news-elon-musks-company-faces-lawsuit-over-fatal-florida-autopilot-crash-2810583'
+
+#Create a function to replace spaces with %20 and that will serve as automated searching for articles
+search_results_for_apple_url ='https://www.ibtimes.com/search/site/apple'
+search_results_for_apple_url_2 = 'https://www.ibtimes.com/search/site/apple%20inc'
+
+
+def main(url):
+    article = scrape_article_from_web(url)
+    article_sentiment_analysis = get_sentiment_analysis(article)
+
+
+main(url)
+
+
+
 
